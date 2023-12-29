715eb386530358c460e09bfd282a240e5349105a
Aeyocca <aeyap42@gmail.com>
2023-12-29 12:11:20 -0500

    remove work

--- work/d1/c1af5e74f11e65bee507d695ca21a8/.command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -euo pipefail
-calc_pav.py --input ERX10632306_ERR11202896.genomecov --bed ERX10632306_ERR11202896.bed \
---depth_threshold 1 \
---cov_threshold 0.8 \
---output ERX10632306_ERR11202896.pav.txt
